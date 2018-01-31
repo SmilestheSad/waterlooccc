bd6dca4f30ed360cac5130832eaeae37425c16a0
David Zheng <david.zheng@cogeco.com>
2018-01-30 19:44:18 -0500

    add ccc sample

--- template/input.py
+++ template/input.py
@@ -29,19 +29,20 @@ vInput= ['1 2 3 4', '9 4 5 6', '8 4 5 6', '0 4 5 6']
 #vInput = readFile('input.txt')
 #Option 3
 #vInput = readInput1()
-print(vInput)
+print(vInput)   # vInput= ['1 2 3 4', '9 4 5 6', '8 4 5 6', '0 4 5 6'] 
 vFirstLine=vInput[0]
 vFirstStringFromFirstLine = vInput[0].split()[0]
 print(vFirstLine,"\n",vFirstStringFromFirstLine)
 
 vInputNew = [ x.split() for x in vInput ]
 vInputNew1 = [ int(x) for y in vInputNew for x in y]
+
 vIntList = []
-for x in vInput:
+for x in vInput:  # x = '1 2 3 4' or '9 4 5 6' ...
     vRowList = []
-    for y in x.split():
-        vRowList.append(int(y))
-    vIntList.append(vRowList)
+    for y in x.split():  # split x from string into a string list
+        vRowList.append(int(y))  # convert to int and append to row list
+    vIntList.append(vRowList)   # Append finished row list to final list
 
 print(vIntList)        
 
